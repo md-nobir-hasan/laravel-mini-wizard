b7d43838132083c0de8f00b566402ae4e98f9fff
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-04-16 17:21:47 +0600

    Update MakeCurd.php

--- src/Console/Commands/MakeCurd.php
+++ src/Console/Commands/MakeCurd.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace nobir\CurdByCommand\Console\Commands;
 
 use Illuminate\Console\Command;
@@ -10,7 +11,17 @@ class MakeCurd extends Command
      *
      * @var string
      */
-    protected $signature = 'nobir';
+    protected $signature = 'nobir:curd';
+
+    // The order of these data type is not change able
+    protected $data_type = [
+            'bigIncrements','bigInteger', 'binary', 'boolean', 'char', 'dateTimeTz', 'dateTime', 'date', 'decimal', 'double', 'enum', 'float', 'foreignId',
+            'foreignIdFor', 'foreignUlid', 'foreignUuid', 'geography', 'geometry', 'id', 'increments', 'integer', 'ipAddress', 'json', 'jsonb', 'longText',
+            'macAddress', 'mediumIncrements', 'mediumInteger', 'mediumText', 'morphs', 'nullableMorphs', 'nullableTimestamps', 'nullableUlidMorphs', 'nullableUuidMorphs',
+            'rememberToken', 'set', 'smallIncrements', 'smallInteger', 'softDeletesTz', 'softDeletes', 'string', 'text', 'timeTz','time','timestampTz',
+            'timestamp','timestampsTz','timestamps','tinyIncrements','tinyInteger','tinyText','unsignedBigInteger','unsignedInteger','unsignedMediumInteger',
+            'unsignedSmallInteger','unsignedTinyInteger','ulidMorphs','uuidMorphs','ulid','uuid', 'year'
+    ];
 
     /**
      * The console command description.
@@ -24,8 +35,28 @@ class MakeCurd extends Command
      */
     public function handle()
     {
-        $this->info('Yes you do it successfully');
+
+        if ($this->confirm('Are you want to add a field?', true)) {
+            //Database field name
+            $n['field_name'] = $this->ask('Field Name:');
+
+            //Data type
+            $n['data_type'] = $this->choice(
+                'Enter a data type?',
+                $this->data_type,
+                'string'
+            );
+
+            //is nullable
+            $n['nullable'] = $this->confirm('Is the field nullable?');
+
+            //default values
+            if($this->confirm('Have any default values?')){
+                $n['default_values'] = $this->ask('Values (Seperate the values using comma):');
+            }
+            
+        }
+
+        $this->info('Process Terminate');
     }
 }
-?>
-

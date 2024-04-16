c526372e09e0fad2179790edadbbe2b225dfbcac
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-04-16 21:29:12 +0600

    migration on the way

--- src/Console/Commands/MakeCurd.php
+++ src/Console/Commands/MakeCurd.php
@@ -11,18 +11,18 @@ class MakeCurd extends Command
      *
      * @var string
      */
-    protected $signature = 'nobir:curd';
+    protected $signature = 'nobir:curd {model}';
 
     // The order of these data type is not change able
     protected $data_type = [
-            'bigIncrements','bigInteger', 'binary', 'boolean', 'char', 'dateTimeTz', 'dateTime', 'date', 'decimal', 'double', 'enum', 'float', 'foreignId',
-            'foreignIdFor', 'foreignUlid', 'foreignUuid', 'geography', 'geometry', 'id', 'increments', 'integer', 'ipAddress', 'json', 'jsonb', 'longText',
-            'macAddress', 'mediumIncrements', 'mediumInteger', 'mediumText', 'morphs', 'nullableMorphs', 'nullableTimestamps', 'nullableUlidMorphs', 'nullableUuidMorphs',
-            'rememberToken', 'set', 'smallIncrements', 'smallInteger', 'softDeletesTz', 'softDeletes', 'string', 'text', 'timeTz','time','timestampTz',
-            'timestamp','timestampsTz','timestamps','tinyIncrements','tinyInteger','tinyText','unsignedBigInteger','unsignedInteger','unsignedMediumInteger',
-            'unsignedSmallInteger','unsignedTinyInteger','ulidMorphs','uuidMorphs','ulid','uuid', 'year'
+        'bigIncrements', 'bigInteger', 'binary', 'boolean', 'char', 'dateTimeTz', 'dateTime', 'date', 'decimal', 'double', 'enum', 'float', 'foreignId',
+        'foreignIdFor', 'foreignUlid', 'foreignUuid', 'geography', 'geometry', 'id', 'increments', 'integer', 'ipAddress', 'json', 'jsonb', 'longText',
+        'macAddress', 'mediumIncrements', 'mediumInteger', 'mediumText', 'morphs', 'nullableMorphs', 'nullableTimestamps', 'nullableUlidMorphs', 'nullableUuidMorphs',
+        'rememberToken', 'set', 'smallIncrements', 'smallInteger', 'softDeletesTz', 'softDeletes', 'string', 'text', 'timeTz', 'time', 'timestampTz',
+        'timestamp', 'timestampsTz', 'timestamps', 'tinyIncrements', 'tinyInteger', 'tinyText', 'unsignedBigInteger', 'unsignedInteger', 'unsignedMediumInteger',
+        'unsignedSmallInteger', 'unsignedTinyInteger', 'ulidMorphs', 'uuidMorphs', 'ulid', 'uuid', 'year'
     ];
-
+    protected $add_field_msg = 'Are you want to add a field?';
     /**
      * The console command description.
      *
@@ -35,28 +35,78 @@ class MakeCurd extends Command
      */
     public function handle()
     {
-
-        if ($this->confirm('Are you want to add a field?', true)) {
-            //Database field name
-            $n['field_name'] = $this->ask('Field Name:');
-
-            //Data type
-            $n['data_type'] = $this->choice(
-                'Enter a data type?',
-                $this->data_type,
-                'string'
-            );
-
-            //is nullable
-            $n['nullable'] = $this->confirm('Is the field nullable?');
-
-            //default values
-            if($this->confirm('Have any default values?')){
-                $n['default_values'] = $this->ask('Values (Seperate the values using comma):');
-            }
-            
+        $model_name = str($this->argument('model'))->title();
+        $table_name = $model_name->lower()->plural();
+        // dd($model_name,$table_name);
+        $data_fields_names = $this->collectFields();
+        // dd($data_fields_names);
+        // Migration file creating
+        if($this->confirm('Are you want to make migration',true)){
+            $this->makeMigration($data_fields_names,$table_name->value());
         }
 
         $this->info('Process Terminate');
+    }
+
+    protected function collectFields()
+    {
+        $i = 0;
+        $n=[];
+        while (true) {
+            if ($this->confirm($this->add_field_msg, true)) {
+                $i++;
+                //Database field name
+                $n[$i]['field_name'] = $this->ask('Field Name:');
+
+                //Data type
+                $n[$i]['data_type'] = $this->choice(
+                    'Enter a data type?',
+                    $this->data_type,
+                    'string'
+                );
+
+                //is nullable
+                $n[$i]['nullable'] = $this->confirm('Is the field nullable?');
+
+                //default values
+                if ($this->confirm('Have any default values?')) {
+                    $n[$i]['default_value'] = $this->ask('Default value is:');
+                }else{
+                    $n[$i]['default_value'] = null;
+
+                }
+                $this->add_field_msg = 'Are you want to add another field?';
+            } else {
+                break;
+            }
+        }
+        return $n;
+    }
+
+    protected function makeMigration($data_fields,$table_name){
+        $stub_content = file_get_contents(__DIR__.'/../../stubs/migration.stub');
+        $content_with_table_name = str_replace('$table_name',$table_name,$stub_content);
+        $fields = '';
+        // if(count($data_fields)>0){
+            foreach($data_fields as $field){
+                $fields .= "\$table->{$field['data_type']}('{$field['field_name']}')";
+                if($field['nullable']){
+                    $fields .= "->nullable()";
+                }
+                if($field['default_value']){
+                    $fields .= "->default({$field['default_value']})";
+                }
+
+                $fields .= ';\n';
+            }
+
+        $content_ready = str_replace('$fields', $fields, $content_with_table_name);
+        $file_name = 'create_'.$table_name.'_table';
+        $file_path = database_path('migrations/'.date('d_m_Y_His').'_'.$file_name.'.php');
+        file_put_contents($file_name,$content_ready);
+        dd($file_path);
+        $this->info("The migration file '$file_name' is created Successfully");
+        // }
+
     }
 }
